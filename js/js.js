04910a41367a0003df7610ef4e21ecab9f205d7c
jason7564 <prez0005@algonquinlive.com>
2015-11-19 20:55:32 -0500

    jQuery Mobile App
    Cheesecake Tutorial

---
+++ js/js.js
@@ -0,0 +1,22 @@
+var counter = 1;
+var pagePart = "#part";
+
+$(document).on("mobileinit", function() {});
+
+$(document).ready(function(ev) {
+    localStorage.setItem("currentPageId", JSON.stringify(pagePart + counter));
+    $("#startTut").on("click", function(ev) {
+        partParse = JSON.parse(localStorage.getItem("currentPageId"));
+        $("#startTut").attr("href", partParse);
+    });
+    $("#part1").on("pageinit", function(ev) {
+        $(".nextPage").on("click", function(ev) {
+            counter += 1;
+            localStorage.setItem("currentPageId", JSON.stringify(pagePart + counter));
+        });
+        $(".prevPage").on("click", function(ev) {
+            counter -= 1;
+            localStorage.setItem("currentPageId", JSON.stringify(pagePart + counter));
+        });
+    });
+});
