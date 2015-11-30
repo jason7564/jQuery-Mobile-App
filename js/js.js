dd3a3c8308b9284c088b893484766397dd626e33
Jason Preziuso <prez0005@algonquinlive.com>
2015-11-30 11:16:29 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,16 +4,19 @@ var pagePart = "#part";
 $(document).on("mobileinit", function() {});
 
 $(document).ready(function(ev) {
-    
-    for (i = 1; i < 10; i++){
-    $("#part"+i).on("pageinit", function(){
-       localStorage.setItem("currentPageId", JSON.stringify("#part"+i));
-       $("#startTut").on("click", function(ev) {
+    localStorage.setItem("currentPageId", JSON.stringify(pagePart + counter));
+    $("#startTut").on("click", function(ev) {
         partParse = JSON.parse(localStorage.getItem("currentPageId"));
         $("#startTut").attr("href", partParse);
-       });
     });
-    };
+    $(".nextPage").on("click", function(ev) {
+        counter += 1;
+        localStorage.setItem("currentPageId", JSON.stringify(pagePart + counter));
+    });
+    $(".prevPage").on("click", function(ev) {
+        counter -= 1;
+        localStorage.setItem("currentPageId", JSON.stringify(pagePart + counter));
+    });
 }):
 
 
